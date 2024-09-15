fae11b9dc263c212243252611ba25860232eb10b
lvkax <sasha.livak22@gmail.com>
2024-09-15 19:47:16 +0300

    Добавьте файлы проекта.

---
+++ Laba33/Main.cs
@@ -0,0 +1,31 @@
+namespace Laba33
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnAddPhone_Click(object sender, EventArgs e)
+        {
+            Phone phone = new Phone();
+            fPhone fp = new fPhone(phone);
+
+            if (fp.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = string.Format("амамоам");
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Припинити роботу застосунку?", "Припинити роботу",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
---
+++ Laba33/Phone.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba33
+{
+    public class Phone
+    {
+        public string Name;
+        public string Model;
+        public string Cost;
+        public string ReleaseYear;
+        public int YearOfPurchase;
+        public string BatteryCapacity;
+        public bool Has3Cameras;
+        public bool HasWirelessCharging;
+
+        public double GaranteEndingYear
+        {
+            get { return getGaranteEndingYear(); }
+
+        }
+        public double getGaranteEndingYear()
+        {
+            if (Name == "iPhone") return YearOfPurchase + 1;
+            else if (Name == "Samsung") return YearOfPurchase + 2;
+            else return YearOfPurchase + 3;
+        }
+    }
+}
